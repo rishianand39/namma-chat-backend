8dff996dcbc05bca46e554a08075cc7736bd1f8f
Rishi Anand <rishi.rn818@gmail.com>
2025-05-04 23:41:57 +0530

    redis credentail removed

--- server/src/config/ioradis.connect.ts
+++ server/src/config/ioradis.connect.ts
@@ -1,9 +1,11 @@
 import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const redis =  new Redis({
-    port : 14863,
-    host : "redis-14863.c232.us-east-1-2.ec2.redns.redis-cloud.com",
-    username: "default",
-    password: "KTUSJrBMFOHjwRRDwbqk4punGy5Bytbd",
-    db: 0,
+    port: parseInt(process.env.REDIS_PORT || "6379", 10),
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    username: process.env.REDIS_USERNAME || "",
+    password: process.env.REDIS_PASSWORD || "",
+    db: parseInt(process.env.REDIS_DB || "0", 10)
   });
